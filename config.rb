a33f637089164d7d4363b97ea1b1013b1bda0ad4
ashwaniweb <ashwaniwebdesigner@gmail.com>
2017-05-08 18:59:15 +0530

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "scss" 
+images_dir = "img" 
+javascripts_dir = "js" 
+# :nested #:compact :expanded :compressed
+output_style = :expanded
+relative_assets = true 
+line_comments = false
