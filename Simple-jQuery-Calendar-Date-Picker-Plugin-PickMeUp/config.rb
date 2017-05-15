de327cb228fddc9f734dd50e9631454428c22773
Ashwani Kumar <ashwaniwebdesigner@gmail.com>
2017-05-15 09:11:30 +0530

    acac

---
+++ Simple-jQuery-Calendar-Date-Picker-Plugin-PickMeUp/config.rb
@@ -0,0 +1,12 @@
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "scss" 
+images_dir = "img" 
+javascripts_dir = "js" 
+# :nested #:compact :expanded :compressed
+output_style = :expanded
+relative_assets = true 
+line_comments = false
+sourcemap = true
+# asset_cache_buster = :none
+# cache = false
